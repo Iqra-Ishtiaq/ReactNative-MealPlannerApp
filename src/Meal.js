6bdad628b99bdb84af9fca69cf0e01f4e3c6965c
Iqra_Ishtiaq <iqra.ishtiaq@divedeepai.com>
2022-03-17 10:39:18 +0500

    updated

--- src/Meal.js
+++ src/Meal.js
@@ -21,7 +21,7 @@ export default function Meal({meal}) {
         <h1>{meal.title}</h1>
         <img src={imageUrl} alt="recipe"/>
     <ul className='instructions'>
-        <li>Preparation time:{meal.readyInMins}minutes </li>
+        <li>Preparation time:{meal.readyInMinutes}minutes </li>
         <li>Number of servings:{meal.servings}</li>
     </ul>
     <a href={meal.sourceUrl}>Go to Recipe</a>
